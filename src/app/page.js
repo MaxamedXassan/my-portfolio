18d5a1ad68d9105811170eddb1b5a530ef74bd20
maxamed xasan <hassanhuriye05@gmail.com>
2023-09-14 17:25:52 +0300

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-   <main>
-     hello
+   <main className="container h-96 bg-white shadow items-center m-auto justify-center flex flex-col">
+      <div className="mb-4">
+        <p className="text-lg flex justify-center gap-1">Hello, i am  <span className="text-orange-600 text-xl font-bold">Moha</span></p> 
+        <h1 className="font-bold md:text-3xl ">Freelancer Next.js/React Developer</h1>
+      </div>
+      <div className="gap-2 p-4  text-white">
+        <button className="mt-2">
+          <Link href={"/projects"} className="bg-orange-400 p-4 rounded mr-2 hover:bg-purple-400">Projects</Link>
+          <Link href={"/contact"} className="bg-slate-400 p-4 rounded  hover:bg-purple-400">Contact</Link>
+        </button>
+      </div>
    </main>
   )
 }
+
