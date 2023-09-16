1f7b4d2043c8930fba56ac4f3c93a7d52609150e
maxamed xasan <hassanhuriye05@gmail.com>
2023-09-16 14:46:47 +0300

    second commit

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -44,20 +44,24 @@ const page = () => {
   return (
     <div className=" ">
       <h1 className="my-4 font-bold text-lg  flex flex-col items-center">Projects</h1>
-      <div className="h-screen grid gap-4  grid-cols-3 mb-6 ml-10">
+      <div className=" flex justify-center ">
+      <div className="h-screen grid gap-24  sm:grid-cols-2 md:grid-cols-3   ">
       {data.map(prject => (
          <div >
          <div>
-         <Image src={prject.img} />
+         <Image src={prject.img} width={240} />
          </div>
             <div>
-            <h1>{prject.title}</h1>           
+            <h1 className="mt-2 flex justify-center font-bold ">{prject.title}</h1>           
             </div>
+            <div  className="bg-orange-400 hover:bg-purple-400 flex justify-center py-2 my-3">
             <button>
               <Link href={prject.view}>View</Link>
             </button>
+            </div>
        </div>
       ))}
+      </div>
       </div>
      
     </div>
